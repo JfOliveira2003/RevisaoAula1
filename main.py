808a5ffe55284d07ad965a574cf48f47baee16b7
kakakaka <kakakakkkka>
2023-06-01 14:53:19 -0300

    Implementando as possibilidades de operação

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 def calculadora(a, b, p)
-	p = input('Qual tipo de operação deseja realizar? ')	
-	
+	p = input('Qual tipo de operação deseja realizar? ').upper()	
+	if p == 'SOMA':
+		print('a soma será: ', a + b)
+	if p == 'SUBTRAÇÃO':
+		print('A subtração será: ', a - b)
+	if p == 'MULTIPLICAÇÃO':
+		print('A multiplicação será: ', a * b)
+	if p == 'DIVISÃO':
+		print('A divisão será: ', a / b)
+
